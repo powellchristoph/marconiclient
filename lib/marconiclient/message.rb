b2375f7f48149da1984d34a9f2aa35e10c8dffac
Chris Powell <powellchristoph@gmail.com>
2014-07-10 11:06:22 -0400

    Framework started. Learning ruby.

---
+++ lib/marconiclient/message.rb
@@ -0,0 +1,3 @@
+module Marconiclient
+
+end
---
+++ lib/marconiclient/queues.rb
@@ -0,0 +1,34 @@
+module Marconiclient
+  class Queue
+
+    def initialize(client, name, auto_create=true)
+      @client = client
+      @name = name
+
+      puts @client
+      puts @name
+
+      if auto_create
+        ensure_exists 
+      end
+    end
+
+    def exists?
+      # Checks if the queue exists
+    end
+
+    def ensure_exists
+      # Ensure the queue exists
+      #
+      # This method is not race safe,
+      # the queue could've been deleted
+      # right after it was called.
+
+      puts 'method called.'
+    end
+
+  end
+end
+
+q = Marconiclient::Queue.new('client', 'name')
+
