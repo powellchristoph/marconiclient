140caacc73b6a14eb5e167aa76e46a0805726830
Chris Powell <powellchristoph@gmail.com>
2014-08-04 14:51:11 -0400

    Add test for queue delete.

--- spec/marconiclient_spec.rb
+++ spec/marconiclient_spec.rb
@@ -2,60 +2,64 @@ require 'spec_helper'
 
 describe "Marconiclient" do
 
-  before do
-    @client = Marconiclient::Client.new(MARCONI_SERVER)
-  end
+  let(:client) { Marconiclient::Client.new(MARCONI_SERVER) }
 
-  describe '#new' do
+  describe 'new client' do
 
     it 'should return a client object' do
-      @client.should be_an_instance_of Marconiclient::Client
+      client.should be_an_instance_of Marconiclient::Client
     end
   end
 
-  describe 'base endpoints' do
+  describe 'has base endpoints' do
     it 'should return the home document' do
-      @client.home.should be_an_instance_of Hash
+      client.home.should be_an_instance_of Hash
     end
 
     it 'should return the health' do
-      @client.health.should be_true
+      client.health.should be_true
     end
   end
 
   describe 'queues endpoints' do
-    describe '#create' do
-      before do
-        @manual_queue = @client.queue('manual_queue', false)
-        @auto_queue = @client.queue('auto_queue')
-      end
+    let(:manual_queue) { client.queue('manual_queue', false) }
+    let(:auto_queue) { client.queue('auto_queue') }
 
-      after do
-        @manual_queue.delete
-        @auto_queue.delete
-      end
-
-      it 'should be a queue' do
-        @manual_queue.should be_an_instance_of Marconiclient::Queue
+    describe 'create queue' do
+      it 'should be a queue object' do
+        manual_queue.should be_an_instance_of Marconiclient::Queue
       end
 
       it 'should not exist on the server' do
-        @manual_queue.exists?.should be_false
+        manual_queue.exists?.should be_false
       end
 
       it 'should exist on server after creation' do
-        @manual_queue.create
-        @manual_queue.exists?.should be_true
+        manual_queue.create
+        manual_queue.exists?.should be_true
       end
 
-      it 'should exist on autocreate' do
-        @auto_queue.exists?.should be_true
+      it 'should exist on server with autocreate' do
+        auto_queue.exists?.should be_true
+      end
+
+      it 'should have proper name' do
+        auto_queue.name.should match('auto_queue')
       end
     end
 
-    describe '#list_queue' do
-      describe 'with empty queues' do
-        let(:resp) { @client.queues }
+    describe 'delete queue' do
+      it 'should delete the queues' do
+        manual_queue.delete
+        auto_queue.delete
+        manual_queue.exists?.should be_false
+        auto_queue.exists?.should be_false
+      end
+    end
+
+    describe 'list queues' do
+      describe 'with no queues on server' do
+        let(:resp) { client.queues }
   
         it 'should return Hash' do
           resp.should be_an_instance_of Hash
@@ -72,17 +76,17 @@ describe "Marconiclient" do
         end
       end
 
-      describe 'with full queues' do
+      describe 'with queues on server' do
         before do
           @queue_list = Array.new
-          (1..5).each { |i| @queue_list << @client.queue("test#{i}") }
+          (1..5).each { |i| @queue_list << client.queue("test#{i}") }
         end
 
         after do
           @queue_list.each { |q| q.delete }
         end
 
-        let(:resp) { @client.queues }
+        let(:resp) { client.queues }
 
         it 'should have links and queues' do
           resp.should have_key(:links)
